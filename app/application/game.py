43cfa56d0b872924af85461631eaf7738fabba49
Zakhar Kokhan <a1793@yandex.ru>
2023-02-22 16:23:02 +0300

    minor changes

--- app/application/game.py
+++ app/application/game.py
@@ -49,7 +49,7 @@ def another_try(session_id, try_number) -> dict:
 
     if sql_response.tries and not sql_response.finish:
         sql_response.tries = sql_response.tries - 1
-        if sql_response.number == int(try_number):
+        if sql_response.number == try_number:
             sql_response.finish = True
         elif sql_response.tries:
             checker = Checker.check(
