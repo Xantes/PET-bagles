e9cdef2056c3efa5753862f234341b11eaad7974
Zakhar Kokhan <a1793@yandex.ru>
2023-02-23 11:13:46 +0300

    add valiidator and little text

--- app/application/forms.py
+++ app/application/forms.py
@@ -1,11 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, SubmitField
-from wtforms.validators import Length, DataRequired
+from wtforms.validators import Length, DataRequired, NumberRange
 
 
 class TryForm(FlaskForm):
     try_number = StringField(label=None,
-                             validators=[Length(min=3, max=3), DataRequired()])
+                             validators=[Length(min=3, max=3), NumberRange(min=0, max=999), DataRequired()])
     submit_button = SubmitField(label='Try')
 
 
